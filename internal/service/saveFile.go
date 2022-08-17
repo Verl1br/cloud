ee1ade273da4fbd643915602e043ad87629e5337
Anton Skakun <antonskakun999@gmail.com>
2022-08-17 16:48:19 +0300

    created saveFile.go

---
+++ internal/service/saveFile.go
@@ -0,0 +1,72 @@
+package service
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/oklog/ulid"
+)
+
+const IMAGE_DIR = "./image"
+
+func saveFile(c *gin.Context) (string, error) {
+	productId, _ := c.GetPostForm("product_id")
+	{
+		if len(productId) == 0 {
+			productId = "default"
+		}
+		err := os.MkdirAll(fmt.Sprintf("%s/product/%s", IMAGE_DIR, productId), os.ModePerm)
+		if err != nil {
+			return "", err
+		}
+	}
+	path := fmt.Sprintf("%s/product/%s", IMAGE_DIR, productId)
+
+	form, _ := c.MultipartForm()
+	var fileName string
+	imgExt := "jpeg"
+	for key := range form.File {
+		fileName = key
+		arr := strings.Split(fileName, ".")
+		if len(arr) > 1 {
+			imgExt = arr[len(arr)-1]
+		}
+		continue
+	}
+
+	file, _, err := c.Request.FormFile(fileName)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return "", err
+	}
+
+	fullFileName := fmt.Sprintf("%s.%s", randomFilename(), imgExt)
+	fileOnDisk, err := os.Create(fmt.Sprintf("%s/%s", path, fullFileName))
+	if err != nil {
+		return "", err
+	}
+	defer fileOnDisk.Close()
+
+	_, err = fileOnDisk.Write(fileBytes)
+	if err != nil {
+		return "", err
+	}
+
+	return fullFileName, nil
+}
+
+func randomFilename() string {
+	t := time.Now()
+	entropy := rand.New(rand.NewSource(t.UnixNano()))
+	return strings.ToLower(fmt.Sprintf("%v", ulid.MustNew(ulid.Timestamp(t), entropy)))
+}
--- internal/service/upload.go
+++ internal/service/upload.go
@@ -2,19 +2,12 @@ package service
 
 import (
 	"fmt"
-	"io/ioutil"
-	"math/rand"
 	"os"
-	"strings"
-	"time"
 
 	"github.com/dhevve/uploadImage/internal/models"
 	"github.com/dhevve/uploadImage/internal/repository"
 	"github.com/gin-gonic/gin"
-	"github.com/oklog/ulid"
 )
-
-const IMAGE_DIR = "./image"
 
 type UploadService struct {
 	repo repository.UploadImage
@@ -25,53 +18,10 @@ func NewUploadService(repo repository.UploadImage) *UploadService {
 }
 
 func (s *UploadService) Upload(c *gin.Context, userId int) (int, error) {
-	productId, _ := c.GetPostForm("product_id")
-	{
-		if len(productId) == 0 {
-			productId = "default"
-		}
-		err := os.MkdirAll(fmt.Sprintf("%s/product/%s", IMAGE_DIR, productId), os.ModePerm)
-		if err != nil {
-			return 0, err
-		}
-	}
-	path := fmt.Sprintf("%s/product/%s", IMAGE_DIR, productId)
-
-	form, _ := c.MultipartForm()
-	var fileName string
-	imgExt := "jpeg"
-	for key := range form.File {
-		fileName = key
-		arr := strings.Split(fileName, ".")
-		if len(arr) > 1 {
-			imgExt = arr[len(arr)-1]
-		}
-		continue
-	}
-
-	file, _, err := c.Request.FormFile(fileName)
+	fullFileName, err := saveFile(c)
 	if err != nil {
 		return 0, err
 	}
-	defer file.Close()
-
-	fileBytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		return 0, err
-	}
-
-	fullFileName := fmt.Sprintf("%s.%s", randomFilename(), imgExt)
-	fileOnDisk, err := os.Create(fmt.Sprintf("%s/%s", path, fullFileName))
-	if err != nil {
-		return 0, err
-	}
-	defer fileOnDisk.Close()
-
-	_, err = fileOnDisk.Write(fileBytes)
-	if err != nil {
-		return 0, err
-	}
-
 	return s.repo.Upload(userId, fullFileName)
 }
 
@@ -92,10 +42,4 @@ func (s *UploadService) Delete(userId, itemId int) error {
 	os.Remove(fmt.Sprintf("%s/%s", path, image.Name))
 
 	return s.repo.Delete(userId, itemId)
-}
-
-func randomFilename() string {
-	t := time.Now()
-	entropy := rand.New(rand.NewSource(t.UnixNano()))
-	return strings.ToLower(fmt.Sprintf("%v", ulid.MustNew(ulid.Timestamp(t), entropy)))
 }
